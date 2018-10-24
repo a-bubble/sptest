a8c18c09e3876a6d50694e9bd2d488ce2911420b
艾营 <923106629@qq.com>
2018-10-24 09:51:32 +0800

    test

---
+++ src/main/java/com/sptest/SptestApplication.java
@@ -0,0 +1,12 @@
+package com.sptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptestApplication.class, args);
+    }
+}
